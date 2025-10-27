61932919a245c323cc31f57d44e58e3ded3eb481
saucerdish <2256589622@qq.com>
2025-10-27 17:40:28 +0800

    finish speed up code,but pgaccess_test can't pass.

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,11 @@ found:
     return 0;
   }
 
+  if((p->usyscall_page = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -134,6 +139,7 @@ found:
     release(&p->lock);
     return 0;
   }
+  p->usyscall_page->pid = p->pid;
 
   // Set up new context to start executing at forkret,
   // which returns to user space.
@@ -153,6 +159,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->usyscall_page)
+    kfree((void*)p->usyscall_page);
+  p->usyscall_page = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -195,6 +204,15 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
+  
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->usyscall_page), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    // uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
 
   return pagetable;
 }
@@ -206,6 +224,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
--- kernel/proc.h
+++ kernel/proc.h
@@ -105,4 +105,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  struct usyscall *usyscall_page;     // 内核分配的用户共享页（存放 struct usyscall）
 };
